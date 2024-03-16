22afeb8dcf212b0080b74fd6203e5aa259b059f8
Mohd Asif <masifabid@outlook.com>
2024-03-16 23:22:11 +0530

    CSS : Make Navbar and DiscountTab fixed

--- app/ui/discountTab.tsx
+++ app/ui/discountTab.tsx
@@ -1,7 +1,7 @@
 import { GrNext, GrPrevious } from "react-icons/gr";
 export const DiscountTab = (props: any) => {
   return (
-    <div className="flex gap-3 bg-gray-300 w-full justify-center pt-1 pb-1">
+    <div className="flex gap-3 bg-gray-300 w-full justify-center pt-1 pb-1 ">
       <button>
         <GrPrevious />
       </button>
