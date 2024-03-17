4fd710cb1d94c393bd1dcf36c8ed7e56b7a57a00
Mohd Asif <masifabid@outlook.com>
2024-03-17 22:34:22 +0530

    Fix the pagination

--- app/ui/pagination.tsx
+++ app/ui/pagination.tsx
@@ -1,19 +1,27 @@
-"use client";
 import { GrNext, GrPrevious } from "react-icons/gr";
 import { PaginationPropsType } from "../lib/definitions";
 
 export const Pagination = (props: PaginationPropsType) => {
-  const { totalRecords, recordsPerPage, currentPage , totalPages} = props;
-  const arr = Array.from(Array(totalPages));
+  const { totalRecords, recordsPerPage, currentPage, totalPages } = props;
 
-  const btnsList = arr?.map((item, index) => {
-    const btnName = index + 1;
-    return (
-      <button className="bg-gray-400 rounded-sm p-1 pl-2 pr-2" key={btnName}>
-        {btnName}
+  let startPage = Math.max(1, currentPage - 3);
+  let endPage = Math.min(totalPages, currentPage + 3);
+
+  if (currentPage <= 3) {
+    endPage = Math.min(7, totalPages);
+  }
+  const btnsList = [];
+  for (let i = startPage; i <= endPage; i++) {
+    btnsList.push(
+      <button
+        key={i}
+        className={` ${i == currentPage ? `text-black` : `text-gray-400`}`}
+        onClick={() => console.log("Clicked page", i)}
+      >
+        {i}
       </button>
     );
-  });
+  }
 
   return (
     <div className="flex gap-3 items-center">
