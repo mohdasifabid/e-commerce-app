89724546a265412ede289d08a2d97c61a47b7f58
Mohd Asif <masifabid@outlook.com>
2024-03-17 20:01:17 +0530

    Replace eye icons with Show Hide text

--- app/ui/input.tsx
+++ app/ui/input.tsx
@@ -2,31 +2,31 @@
 
 import { useState } from "react";
 import { InputPropsType } from "../lib/definitions";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 export const Input = (props: InputPropsType) => {
   const { placeholder, label, type, setter } = props;
   const [showPassword, setShowPassword] = useState(false);
 
   const handlePasswordVisibility = () =>
     setShowPassword((prevState) => !prevState);
-    
+
   return (
     <div className="min-w-300 h-74">
       <p className="">{label}</p>
       <div className="relative">
         <input
           onChange={(e) => setter(e.target.value)}
-          type={showPassword ? "text" : type} 
+          type={showPassword ? "text" : type}
           placeholder={placeholder}
           className="w-456 h-48 p-2  focus:outline-none border-2 border-gray-400 rounded-md"
         />
         {type === "password" && (
           <button
             type="button"
-            className="absolute top-4 right-4"
+            className="absolute top-3 right-4 underline"
             onClick={handlePasswordVisibility}
           >
-            {showPassword ? <FaEyeSlash /> : <FaEye />}
+            {showPassword ? "Hide" : "Show"}
           </button>
         )}
       </div>
