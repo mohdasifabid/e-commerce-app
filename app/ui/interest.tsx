c328030694995bbc49e59fedae1fa8a65cb479a5
Mohd Asif <masifabid@outlook.com>
2024-03-17 23:31:38 +0530

    Handle checked and unchecked func

--- app/ui/interest.tsx
+++ app/ui/interest.tsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import { InterestPropsTypes } from "../lib/definitions";
 
 export const Interest = (props: InterestPropsTypes) => {
   const { interest, checked, value } = props;
+  const [isInterestChecked, setIsInterestChecked] = useState(checked);
+
   return (
     <div className="flex gap-5 items-center">
       <input
         type="checkbox"
         className="w-6 h-6"
-        checked={checked}
+        checked={isInterestChecked}
         value={value}
+        onChange={(e) => {
+          setIsInterestChecked((prevState) => !prevState);
+        }}
       />
       <p>{interest}</p>
     </div>
-    
   );
 };
