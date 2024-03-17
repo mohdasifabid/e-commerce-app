c67b4f5570eca6e2aaeaf1c3710fcff218a6984d
Mohd Asif <masifabid@outlook.com>
2024-03-17 17:02:41 +0530

    Handle password visibility

--- app/ui/input.tsx
+++ app/ui/input.tsx
@@ -1,16 +1,35 @@
-import { InputPropsType } from "../lib/definitions";
+"use client";
 
+import { useState } from "react";
+import { InputPropsType } from "../lib/definitions";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 export const Input = (props: InputPropsType) => {
   const { placeholder, label, type, setter } = props;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordVisibility = () =>
+    setShowPassword((prevState) => !prevState);
+    
   return (
     <div className="min-w-300 h-74">
       <p className="">{label}</p>
-      <input
-        onChange={(e) => setter(e.target.value)}
-        type={type}
-        placeholder={placeholder}
-        className="w-456 h-48 p-2  focus:outline-none border-2 border-gray-400 rounded-md"
-      />
+      <div className="relative">
+        <input
+          onChange={(e) => setter(e.target.value)}
+          type={showPassword ? "text" : type} 
+          placeholder={placeholder}
+          className="w-456 h-48 p-2  focus:outline-none border-2 border-gray-400 rounded-md"
+        />
+        {type === "password" && (
+          <button
+            type="button"
+            className="absolute top-4 right-4"
+            onClick={handlePasswordVisibility}
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
