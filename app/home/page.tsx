bb78ad627504cabe0869c7b75d2cf41c8f71f464
Mohd Asif <masifabid@outlook.com>
2024-03-17 16:42:37 +0530

    Add Basic str for HOme

---
+++ app/home/page.tsx
@@ -0,0 +1,16 @@
+import { Layout } from "../ui/layout";
+import { SignUp } from "../ui/signUp";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="flex flex-col h-40">
+        <div className="flex-grow">
+          <p className="text-4xl font-bold mb-4 pt-4">Home</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
