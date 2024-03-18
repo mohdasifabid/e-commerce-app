60dd735baf53ccb804c7b377c8fd8758e6782ee9
Mohd Asif <masifabid@outlook.com>
2024-03-18 21:50:19 +0530

    CSS : adjust singuUp's layout

--- app/ui/signUp.tsx
+++ app/ui/signUp.tsx
@@ -28,16 +28,16 @@ export const SignUp = (props: any) => {
       email,
       password,
     });
-    if(res.status === 201){
-      localStorage.setItem("authToken",res.data.token)
-      localStorage.setItem("userName",res.data.newUser.name)
-      router.push("/home")
+    if (res.status === 201) {
+      localStorage.setItem("authToken", res.data.token);
+      localStorage.setItem("userName", res.data.newUser.name);
+      router.push("/home");
     }
   };
   return (
-    <div className="flex flex-col items-center border-2 border-gray-400 rounded-xl pl-12 pr-12 pb-4 w-576">
-      <p className="text-4xl font-bold pb-6 pt-8">Create your account</p>
-      <div className="flex flex-col gap-8">
+    <div className="flex flex-col items-center border-2 border-gray-400 rounded-xl pl-12 pr-12 pb-4 w-576 h-614">
+      <p className="text-4xl font-600 pb-6 pt-16">Create your account</p>
+      <div className="flex flex-col gap-10">
         <Input
           placeholder="Please enter name"
           label="Name"
@@ -57,8 +57,10 @@ export const SignUp = (props: any) => {
           type="password"
           setter={setPassword}
         />
-        <Button btnName="CREATE ACCOUNT" onClick={createAccountHandler} />
-        <hr />
+        <span className="pt-2">
+          <Button btnName="CREATE ACCOUNT" onClick={createAccountHandler} />
+        </span>
+        <hr/>
         <p className="flex justify-center pb-3 gap-3">
           Have an Account?{" "}
           <a className="cursor-pointer" onClick={handleNavigationToSignInPage}>
