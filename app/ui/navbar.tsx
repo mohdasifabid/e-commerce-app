cd0acc3fa472338ff7d7fd5543bcfc70e6ed718f
Mohd Asif <masifabid@outlook.com>
2024-03-18 21:20:55 +0530

    CSS : Change font-weight, icon

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { FaSearch, FaShoppingCart } from "react-icons/fa";
+import { IoIosSearch } from "react-icons/io";
+import { BsCart2 } from "react-icons/bs";
 
 import useAuth from "../lib/useAuth";
 import { DiscountBar } from "./discountBar";
 import loginLogoutHandler from "../lib/loginLogoutHandler";
+
 
 export const Navbar = (props: any) => {
   const router = useRouter();
@@ -23,7 +25,7 @@ export const Navbar = (props: any) => {
   const userInfo = userInfoString ? JSON.parse(userInfoString) : null;
 
   return (
-    <div className="fixed top-0 left-0 bg-white z-10 w-full h-18">
+    <div className="fixed top-0 left-0 bg-white z-10 w-full h-100">
       <div className="flex flex-col border-2 w-full gap-4 pl-7 pr-7 ">
         <div className="flex justify-end pt-2">
           <div className="flex gap-7 ">
@@ -36,20 +38,35 @@ export const Navbar = (props: any) => {
           </div>
         </div>
         <div className="flex justify-between pb-2">
-          <p className="text-2xl font-bold">E-COMMERCE</p>
+          <p className="text-2xl font-bold leading-10 text-left font-inter">
+            E-COMMERCE
+          </p>
           <div className="flex gap-6 text-1xl font-bold">
-            <button onClick={handleCategoryClick}>Categories</button>
-            <button>Sale</button>
-            <button>Clearance</button>
-            <button>New Stock</button>
-            <button>Trending</button>
+            <button
+              className="font-inter text-base font-semibold leading-5 text-left"
+              onClick={handleCategoryClick}
+            >
+              Categories
+            </button>
+            <button className="font-inter text-base font-semibold leading-5 text-left">
+              Sale
+            </button>
+            <button className="font-inter text-base font-semibold leading-5 text-left">
+              Clearance
+            </button>
+            <button className="font-inter text-base font-semibold leading-5 text-left">
+              New Stock
+            </button>
+            <button className="font-inter text-base font-semibold leading-5 text-left">
+              Trending
+            </button>
           </div>
           <div className="flex gap-4">
             <button>
-              <FaSearch className="text-xl " />
+              <IoIosSearch className="text-xl " />
             </button>
             <button>
-              <FaShoppingCart className="text-xl" />
+              <BsCart2 className="text-xl" />
             </button>
           </div>
         </div>
