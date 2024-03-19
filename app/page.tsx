e060f09433c85eb71c053a6573e146729577ca68
Mohd Asif <masifabid@outlook.com>
2024-03-19 22:09:42 +0530

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,15 @@ export default function Home() {
   const router = useRouter();
   const isLoggedIn = useAuth();
 
-  // useEffect(() => {
-  //   if (isLoggedIn) {
-  //     router.push("/categories");
-  //   } else {
-  //     router.push("/login");
-  //   }
-  // }, [isLoggedIn]);
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push("/categories");
+    } else {
+      router.push("/login");
+    }
+  }, [isLoggedIn]);
   return (
     <main className="flex min-h-screen min-w-screen bg-white">
-      <Loader />
     </main>
   );
 }
