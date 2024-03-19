d6e423c791b4d18a6efc62611070919a494747e8
Mohd Asif <masifabid@outlook.com>
2024-03-19 22:09:02 +0530

    Add Loader

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,22 @@ import { useRouter } from "next/navigation";
 import InterestPage from "./categories/page";
 import useAuth from "./lib/useAuth";
 import { useEffect } from "react";
+import Loader from "./loader";
 
 export default function Home() {
   const router = useRouter();
   const isLoggedIn = useAuth();
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      router.push("/categories");
-    } else {
-      router.push("/login");
-    }
-  }, [isLoggedIn]);
-  return <main className="flex min-h-screen min-w-screen bg-white"></main>;
+  // useEffect(() => {
+  //   if (isLoggedIn) {
+  //     router.push("/categories");
+  //   } else {
+  //     router.push("/login");
+  //   }
+  // }, [isLoggedIn]);
+  return (
+    <main className="flex min-h-screen min-w-screen bg-white">
+      <Loader />
+    </main>
+  );
 }
